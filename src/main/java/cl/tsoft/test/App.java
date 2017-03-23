3660334f09b22e705ede21a87fdde3231a59bfad
Paulo I. Carrasco González <paulo.carrasco.gonzalez@gmail.com>
2017-03-23 19:46:11 -0300

    test

--- src/main/java/cl/tsoft/test/App.java
+++ src/main/java/cl/tsoft/test/App.java
@@ -1,11 +1,11 @@
 package cl.tsoft.test;
 
 import java.util.UUID;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
@@ -18,6 +18,14 @@ public class App
 
     	String id = UUID.randomUUID().toString();
     	return id;
-
+    }
+    
+    public void dateTimeLocal() {
+    	Date date = new Date();
+    	
+    	//Caso 1: obtener la hora y salida por pantalla con formato:
+    	DateFormat hourFormat = new SimpleDateFormat("HH:mm:ss");
+    	System.out.println("Hora: "+hourFormat.format(date));
+    	
     }
 }
