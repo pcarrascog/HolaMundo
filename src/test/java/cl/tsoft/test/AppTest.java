affe3004098d578276c7f31c8e4817b7f6c9d381
Paulo I. Carrasco González <paulo.carrasco.gonzalez@gmail.com>
2017-03-30 10:08:30 -0300

    acl test

--- src/test/java/cl/tsoft/test/AppTest.java
+++ src/test/java/cl/tsoft/test/AppTest.java
@@ -3,11 +3,26 @@ package cl.tsoft.test;
 import org.junit.Assert;
 import org.junit.Test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.ChromeDriver;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-
+	
+	   @Test
+	   public void openBlazeMeterTest() {
+	       Chromedriver driver = new ChromeDriver();
+	       driver.get("http://blazemeter.com");
+	       WebElement title = driver.findElement(By.xpath("//title"));
+	       System.out.println("********************************************************");
+	       System.out.println("*" + title.getAttribute("text") + "*");
+	       System.out.println("********************************************************");
+	       driver.quit();
+	   }
+/*
 	@Test
 	public void testLengthOfTheUniqueKey() {
 
@@ -27,7 +42,7 @@ public class AppTest {
 		App obj = new App();
 		Assert.assertEquals(36, obj.generateUniqueKey().length());
 	}	
-/*	
+
 	@Test
 	public void testOfKeyisDistinct() {
 		App obj = new App();
